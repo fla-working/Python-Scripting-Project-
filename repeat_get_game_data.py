f2ca1cd515cd90fc407135bb7d48f9566c22ee08
Forte Flavio <flavioworking10@gmail.com>
2024-01-23 11:29:08 +0100

    change name file

--- repeat_get_game_data.py
+++ repeat_get_game_data.py
@@ -5,6 +5,10 @@ from subprocess import PIPE, run
 import sys
 
 
+GAME_DIR_PATTERN = "game"
+GAME_CODE_EXTENSION = ".go"
+GAME_COMPILE_COMMAND = ["go", "build"]
+
 #
 def find_all_paths(source):
     game_paths = []
@@ -42,6 +46,49 @@ def copy_and_overwrite(source, dest):
         shutil.rmtree(dest)
     shutil.copytree(source, dest)
 
+
+#
+def make_json_metadata_file(path, game_dirs):
+    data = {
+        "gameNames": game_dirs,
+        "numberOfGames": len(game_dirs)
+    }
+    with open(path, 'w') as f:
+        json.dump(data, f)
+
+
+#
+def compile_game_code(path):
+    code_file_name = None
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith(GAME_CODE_EXTENSION):
+                code_file_name = file
+                
+                break
+        
+        break
+
+    if code_file_name is None:
+        return
+    
+    command = GAME_COMPILE_COMMAND + [code_file_name]
+    run_command(command, path)
+
+
+
+
+#
+def run_command(command, path):
+    cwd = os.getcwd()
+    os.chdir(path)
+
+    result = run(command, stdout=PIPE, stdin=PIPE, universal_newlines=True)
+    print("compile result: ", result)
+
+    os.chdir(cwd)
+
+
 # 
 def main(source, target):
     cwd = os.getcwd()
@@ -52,14 +99,17 @@ def main(source, target):
     new_game_dirs = get_name_from_paths(game_paths, "_game")
     
     create_dir(target_path)
-    
+
     for src, dest in zip(game_paths, new_game_dirs):
         dest_path = os.path.join(target_path, dest)
         copy_and_overwrite(src, dest_path)
+        compile_game_code(dest_path)
+
+    json_path = os.path.join(target_path, "metadata.json")
+    make_json_metadata_file(json_path, new_game_dirs)
 
 
 
-GAME_DIR_PATTERN = "game"
 
   
 
